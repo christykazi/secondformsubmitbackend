2e57bb46bb50ba883ebd266f6555177f9825a902
christykazi <christyagbai4178@gmail.com>
2024-01-12 21:31:36 +0100

    mongodb installation

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const express = require("expree")
+const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 app.use(express.json())
 
+const mongoUrl = "mongodb://127.0.0.1:27017/crixianna"
 
+mongoose.connect(mongoUrl, 
+    {useNewUrlParser: true,} )
+.then(() =>{
+    console.log("connected to database")
+} )
+.catch((e) => console.log(e))
 
-mongoose.connect(mongourl, {useNewUrlParser: true,} )
+app.listen(5000, () => {
+    console.log("server started")
+})
